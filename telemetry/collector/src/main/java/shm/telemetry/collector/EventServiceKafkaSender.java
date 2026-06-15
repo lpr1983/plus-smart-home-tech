4449812f14279aa0a9e1cf793c702d2b353796e5
Leonid Proskuryakov <lpr@inbox.ru>
2026-06-15 09:33:03 +0300

    Перенес имена топиков в properties.

--- telemetry/collector/src/main/java/shm/telemetry/collector/EventServiceKafkaSender.java
+++ telemetry/collector/src/main/java/shm/telemetry/collector/EventServiceKafkaSender.java
@@ -3,6 +3,7 @@ package shm.telemetry.collector;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
@@ -14,17 +15,22 @@ import shm.telemetry.collector.model.sensor.BaseSensorEvent;
 @Service
 public class EventServiceKafkaSender implements EventService {
     private final Producer<Void, SpecificRecordBase> kafkaProducer;
+    private final String sensorEventsTopic;
+    private final String hubEventsTopic;
 
-    public EventServiceKafkaSender(Producer<Void, SpecificRecordBase> producer) {
+    public EventServiceKafkaSender(Producer<Void, SpecificRecordBase> producer,
+                                   @Value("${kafka.topic.sensor-events}") String sensorEventsTopic,
+                                   @Value("${kafka.topic.hub-events}") String hubEventsTopic) {
         this.kafkaProducer = producer;
+        this.sensorEventsTopic = sensorEventsTopic;
+        this.hubEventsTopic = hubEventsTopic;
     }
 
     @Override
     public void sendSensorEvent(BaseSensorEvent sensorEvent) {
         SensorEventAvro sensorEventAvro = AvroSensorEventMapper.sensorEventToAvro(sensorEvent);
 
-        String topic = "telemetry.sensors.v1";
-        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(topic, sensorEventAvro);
+        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(sensorEventsTopic, sensorEventAvro);
 
         kafkaProducer.send(record);
     }
@@ -33,8 +39,7 @@ public class EventServiceKafkaSender implements EventService {
     public void sendHubEvent(BaseHubEvent hubEvent) {
         HubEventAvro hubEventAvro = AvroHubEventMapper.hubEventToAvro(hubEvent);
 
-        String topic = "telemetry.hubs.v1";
-        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(topic, hubEventAvro);
+        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(hubEventsTopic, hubEventAvro);
 
         kafkaProducer.send(record);
     }
