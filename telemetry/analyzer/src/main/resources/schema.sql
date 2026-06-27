f284cbcac289450eda3506e4a5720638f5d2461b
Leonid Proskuryakov <lpr@inbox.ru>
2026-06-27 16:44:13 +0300

    Исправление схемы для запуска сервиса.

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -1,10 +1,10 @@
----- создаём таблицу scenarios
---CREATE TABLE IF NOT EXISTS scenarios (
---    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---    hub_id VARCHAR,
---    name VARCHAR,
---    UNIQUE(hub_id, name)
---);
+-- создаём таблицу scenarios
+CREATE TABLE IF NOT EXISTS scenarios (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    hub_id VARCHAR,
+    name VARCHAR,
+    UNIQUE(hub_id, name)
+);
 --
 ---- создаём таблицу sensors
 --CREATE TABLE IF NOT EXISTS sensors (
