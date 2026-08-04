aa3ab9112d6216579fffdec27e826de4aad9dccf
lpr1983 <lpr@inbox.ru>
2026-08-04 20:09:06 +0300

    Добавление RequestInterceptor для feign-клиентов order-service.

---
+++ commerce/order-service/src/main/java/ru/yandex/practicum/order/config/FeignConfig.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.order.config;
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor sourceServiceHeaderInterceptor(
+            @Value("${spring.application.name}") String applicationName
+    ) {
+        return template -> template.header("X-Source-Service", applicationName);
+    }
+}
