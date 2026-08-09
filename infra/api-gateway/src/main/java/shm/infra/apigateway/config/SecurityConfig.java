7c3056f9428671912a96889c93c1c82dfaddbb5f
lpr1983 <lpr@inbox.ru>
2026-08-09 16:49:27 +0300

    Подключение spring-boot-starter-security, добавление публичного SecurityWebFilterChain, тестового  MapReactiveUserDetailsService с двумя пользователями.

---
+++ infra/api-gateway/src/main/java/shm/infra/apigateway/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package shm.infra.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        return new MapReactiveUserDetailsService(
+                User.builder()
+                        .username("ivan")
+                        .password(passwordEncoder.encode("ivan"))
+                        .roles("USER")
+                        .build(),
+                User.builder()
+                        .username("anna")
+                        .password(passwordEncoder.encode("anna"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .pathMatchers(
+                                HttpMethod.GET,
+                                "/api/products/**",
+                                "/api/categories/**",
+                                "/api/inventory/**"
+                        ).permitAll()
+                        .anyExchange().denyAll()
+                )
+                .build();
+    }
+}
